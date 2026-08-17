1e84404af4f14dd6ea7a5eacaa0c684594520dbe
Nayem Ahmed <nayemk0087@gmail.com>
2026-08-17 16:23:27 +0600

    added recipe collection section featuring 4 responsive cards, metadata badges, and a floating animated 'Browse All Recipes' CTA button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Banner from "@/components/Banner";
 import PersonalizationSection from "@/components/PersonalizationSection";
 import CounterProps from "@/components/CounterProps";
 import HowItWorksSection from "@/components/HowItWorksSection";
+import RecipeCollectionSection from "@/components/RecipeCollection";
 
 
 export default function Home() {
   return (
     <div>
       <Banner />
+      <RecipeCollectionSection/>
       <PersonalizationSection />
       <CounterProps />
       <HowItWorksSection />
